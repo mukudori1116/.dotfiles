3795d0bf2f5b50682a9b7bdedd07544a3c7439c4
mukudori1116 <ryotaf1116@gmail.com>
2021-02-17 05:05:40 +0000

    2021/02/18

--- .zsh/tools.zsh
+++ .zsh/tools.zsh
@@ -1,3 +1,14 @@
+# less
+export PAGER="less"
+export LESS_TERMCAP_mb=$'\e[1;32m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;31m'
+export GROFF_NO_SGR=1                  # for konsole and gnome-terminal
+
 # tmux
 export TERM=xterm-256color
 
